57fe88c5b9b1b9cce150b7d9e3db20dd5afbf24f
Qiu_Mi <beafraid@163.com>
2022-09-03 23:25:38 +0800

    Upload Golden Hoop item.

--- Assets/Items/Golden Hoop/Scripts/Hoop_Controller.cs
+++ Assets/Items/Golden Hoop/Scripts/Hoop_Controller.cs
@@ -6,27 +6,48 @@ using UnityEngine;
 
 public class Hoop_Controller : MonoBehaviour
 {
-    public List<Vector3> listDefaultNodePosition = new List<Vector3>();
-    public List<Vector3> listSqueezeNodePosition = new List<Vector3>();
-
     public Spline spline_Left;
+    public Spline spline_Right;
+
+    [Header("Cache")]
+    public List<SplineNode> listDefaultNode = new List<SplineNode>();
+    public List<SplineNode> listSqueezeNode = new List<SplineNode>();
+
+    #region Invoke by extern (CIB/CSB)
+    public void OnMouseDown(bool isDown)
+    {
+        //PS:因为SplineNode里面是通过调用event进行更新，因此不能直接赋值
+        for (int i = 0; i != spline_Left.nodes.Count; i++)
+        {
+            var left_node = spline_Left.nodes[i];
+            var right_node = spline_Right.nodes[i];
+            left_node.Position = right_node.Position = isDown ? listSqueezeNode[i].Position : listDefaultNode[i].Position;
+            left_node.Direction = right_node.Direction = isDown ? listSqueezeNode[i].Direction : listDefaultNode[i].Direction;
+        }
+    }
+
+    public void OnSqueezeProcess(float percent)
+    {
+        for (int i = 0; i != spline_Left.nodes.Count; i++)
+        {
+            var left_node = spline_Left.nodes[i];
+            var right_node = spline_Right.nodes[i];
+            left_node.Position = right_node.Position = Vector3.Lerp(listDefaultNode[i].Position, listSqueezeNode[i].Position, percent);
+            left_node.Direction = right_node.Direction = Vector3.Lerp(listDefaultNode[i].Direction, listSqueezeNode[i].Direction, percent);
+        }
+    }
+    #endregion
+
+#if UNITY_EDITOR
     [Button("SaveDefaultInfo")]
     public void SaveDefaultInfo()
     {
-        listDefaultNodePosition.Clear();
-        foreach (var node in spline_Left.nodes)
-        {
-            listDefaultNodePosition.Add(node.Position);
-        }
+        listDefaultNode = spline_Left.nodes;
     }
     [Button("SaveSqueezeInfo")]
     public void SaveSqueezeInfo()
     {
-        listSqueezeNodePosition.Clear();
-        foreach (var node in spline_Left.nodes)
-        {
-            listSqueezeNodePosition.Add(node.Position);
-        }
+        listSqueezeNode = spline_Left.nodes;
     }
-
+#endif
 }
