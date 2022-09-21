9c47c8ba4ccd0ba979c69224f999fe71ff3c1d53
Qiu_Mi <beafraid@163.com>
2022-09-21 23:58:07 +0800

    同上

--- Assets/Items/Snake/Scripts/Snake_Controller.cs
+++ Assets/Items/Snake/Scripts/Snake_Controller.cs
@@ -2,10 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
-
+using Threeyes.EventPlayer;
 public class Snake_Controller : MonoBehaviour
 {
-    public UnityEvent onEyeBlink = new UnityEvent();//眨眼
+    //public UnityEvent onEyeBlink = new UnityEvent();//眨眼（PS:因为UMod导出后的事件有问题，所以暂时不使用该方案）
+    public List<EventPlayer_SOAction> listEventPlayerSOAction = new List<EventPlayer_SOAction>();
     public Vector2 blinkIntervalDurationRange = new Vector2(3, 5);
 
     [Header("Runtime")]
@@ -15,7 +16,8 @@ public class Snake_Controller : MonoBehaviour
     {
         if (Time.time - lastBlinkTime > nextblinkIntervalDuration)
         {
-            onEyeBlink.Invoke();
+            //onEyeBlink.Invoke();
+            listEventPlayerSOAction.ForEach(ep => ep.Play());
             nextblinkIntervalDuration = Random.Range(blinkIntervalDurationRange.x, blinkIntervalDurationRange.y);
             lastBlinkTime = Time.time;
         }
