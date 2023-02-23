2a44b5dd1eadd44394fa72c2fb56e9eace79ca65
QiuHanWen <beafraid@163.com>
2023-02-23 17:43:56 +0800

    Update Particles and SpaceShip Item.

--- Assets/Items/Particles/VFX/Input/Pyro/PyroLauncher.cs
+++ Assets/Items/Particles/VFX/Input/Pyro/PyroLauncher.cs
@@ -2,19 +2,21 @@ using UnityEngine;
 using UnityEngine.VFX;
 public class PyroLauncher : MonoBehaviour
 {
+    public Transform tfSpawnTarget;
     [SerializeField] VisualEffect _vfx = null;
 
     VFXEventAttribute _attrib;
 
+    //PS:UMod有识别事件错误的问题，因此只能通过唯一方法调用
+    public void OnButtonDownUp(bool isDown)
+    {
+        if (isDown)
+            Launch();
+    }
     public void Launch()
     {
-        Camera _camera = AC_ManagerHolder.EnvironmentManager.MainCamera;
-        var pos = AC_ManagerHolder.SystemCursorManager.MousePosition;
-        pos.z = _vfx.transform.position.z - _camera.transform.position.z;
-        pos = _camera.ScreenToWorldPoint(pos);
-
         if (_attrib == null) _attrib = _vfx.CreateVFXEventAttribute();
-        _attrib.SetVector3("position", pos);
+        _attrib.SetVector3("position", tfSpawnTarget.position);
 
         _vfx.SendEvent("OnManualSpawn", _attrib);//发送事件
     }
