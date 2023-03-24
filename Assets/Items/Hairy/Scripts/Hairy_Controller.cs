81ee2a0af0f547fda7270df9bf8b634638a28a11
QiuHanWen <beafraid@163.com>
2023-03-24 18:42:04 +0800

    Update Items.

--- Assets/Items/Hairy/Scripts/Hairy_Controller.cs
+++ Assets/Items/Hairy/Scripts/Hairy_Controller.cs
@@ -1,6 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.DemoTeam.Hair;
+using static Unity.DemoTeam.Hair.HairInstance;
+using Threeyes.Coroutine;
+using static Unity.DemoTeam.Hair.HairAsset;
 //using Unity.DemoTeam.Hair;/ToFix：/无法引用的原因可能是其Assembled文件丢失了部分引用
 
 /// <summary>
@@ -13,6 +17,8 @@ public class Hairy_Controller : MonoBehaviour
 {
     public Vector3 relateWindForce = new Vector3(0, -1, 0);
     public AC_HairInstanceController hairInstanceController;
+
+    public List<HairAsset> listHairAsset = new List<HairAsset>();
 
     #region Callback
     //Invoke by CIB
@@ -38,10 +44,39 @@ public class Hairy_Controller : MonoBehaviour
                 hairInstanceController.Config.gravity = 1;
                 isBoredState = false;
             }
-
             hairInstanceController.UpdateSetting();
         }
+    }
 
+    public HairAsset hairAsset;
+    //Invoked by PD
+    public void OnHairLengthChanged(int index)
+    {
+        //if (index + 1 >= listHairAsset.Count)
+        //    return;
+        GroupInstance[] arrInstance = hairInstanceController.Comp.strandGroupInstances;
+        GroupInstance groupInstanceFirst = arrInstance[0];
+        groupInstanceFirst.groupAssetReference = new GroupAssetReference() { hairAsset = listHairAsset[index] };
+        arrInstance[0] = groupInstanceFirst;
+        hairInstanceController.Comp.strandGroupInstances = arrInstance;
+
+        ////运行时更新【Bug：0.9版本会导致闪退，0.10可用。暂时无法使用，因为只能通过编辑器代码BuildHairAsset。解决后就可以直接暴露其所有参数，替换掉上面的死板实现】
+        //SettingsProcedural settingsProcedural = hairAsset.settingsProcedural;
+        //settingsProcedural.strandLength = index * 0.5f;
+        //hairAsset.settingsProcedural = settingsProcedural;
+        //HairAssetBuilder.BuildHairAsset(hairAsset);//【Warning：仅编辑器可用】
+
+        ReBuild();//重新刷新
+    }
+    void ReBuild()
+    {
+        CoroutineManager.StartCoroutineEx(IEReBuild());
+    }
+    private IEnumerator IEReBuild()
+    {
+        hairInstanceController.Comp.enabled = false;
+        yield return null;
+        hairInstanceController.Comp.enabled = true;
     }
     #endregion
 
